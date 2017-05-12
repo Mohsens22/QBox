385b74ca04723c9671d0b5a2934319ce5ae885d4
@Mohsens22 <mohsens2@outlook.com>
2017-05-12 22:19:07 +0430

    Fixed DB bug

--- QBox/Model/SetNewQuestions.cs
+++ QBox/Model/SetNewQuestions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace Model
             {
                
                 db.Questions.Add(question);
+                db.Entry(question.Course).State = EntityState.Unchanged;
                 db.SaveChanges();
                 
             }
