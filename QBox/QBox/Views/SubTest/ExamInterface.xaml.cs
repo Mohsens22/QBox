de7a3bccae59db87d94048a757c9bd8da076782c
@Mohsens22 <mohsens2@outlook.com>
2017-05-17 08:06:29 +0430

    :)

--- QBox/QBox/Views/SubTest/ExamInterface.xaml.cs
+++ QBox/QBox/Views/SubTest/ExamInterface.xaml.cs
@@ -24,6 +24,7 @@ namespace QBox.Views.SubTest
     public sealed partial class ExamInterface : Page
     {
         ObservableCollection<Model.Question> Questions = new ObservableCollection<Model.Question>();
+        private Random _random = new Random();
         public ExamInterface()
         {
             this.InitializeComponent();
@@ -32,7 +33,25 @@ namespace QBox.Views.SubTest
         {
             base.OnNavigatedTo(e);
           var x=  e.Parameter as Classes.CourseItems;
-            var y = Model.SetNewQuestions.GetQuestion(x.Dars, x.Items);
+            for (int i = 0; i < x.Items; i++)
+            {
+                Questions.Add(new Model.Question
+                {
+                    Soal = "Soal with the index of" + i,
+                    Answer = new List<Model.Answer>
+                    {
+
+                         new Model.Answer{AssignedFlag=1,Javab="1St answer for"+i },
+                         new Model.Answer{AssignedFlag=2,Javab="2Nd answer for"+i },
+                         new Model.Answer{AssignedFlag=3,Javab="3Rd answer for"+i },
+                         new Model.Answer{AssignedFlag=4,Javab="4Th answer for"+i },
+                    },
+                    Correct = _random.Next(1, 4),
+                    Course = x.Dars
+
+
+                });
+            }
         }
     }
 }
